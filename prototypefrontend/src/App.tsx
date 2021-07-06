29b05011c1020bed108235bf902e47eebe3e8191
hyesung oh <haesungoh414@gmail.com>
2021-07-06 18:48:23 +0900

    update: install tensorflow-models/posenet

--- prototypefrontend/src/App.tsx
+++ prototypefrontend/src/App.tsx
@@ -4,6 +4,13 @@ function App() {
     return (
         <div>
             <h1>Prototype</h1>
+            <video
+                autoPlay
+                playsInline
+                muted={true}
+                height="100vh"
+                width="100vw"
+            ></video>
         </div>
     );
 }
