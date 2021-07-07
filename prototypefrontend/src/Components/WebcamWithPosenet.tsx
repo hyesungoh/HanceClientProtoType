c8b7af44e8204973fdb65c03a69cf31414b48396
hyesung oh <haesungoh414@gmail.com>
2021-07-07 23:40:51 +0900

    feat: apply webcam with react-webcam

--- prototypefrontend/src/Components/WebcamWithPosenet.tsx
+++ prototypefrontend/src/Components/WebcamWithPosenet.tsx
@@ -1,22 +1,66 @@
-// async function estimatePoseOnImage(imageElement) {
-//     // load the posenet model from a checkpoint
-//     const net = await posenet.load();
+import { useRef } from "react";
 
-//     const pose = await net.estimateSinglePose(imageElement, {
-//       flipHorizontal: false
-//     });
-//     return pose;
-//   }
-
-//   const imageElement = document.getElementById('cat');
-
-//   const pose = estimatePoseOnImage(imageElement);
-
-//   console.log(pose);
+import * as tf from "@tensorflow/tfjs";
+import * as posenet from "@tensorflow-models/posenet";
+import Webcam from "react-webcam";
 
 const WebcamWithPosenet = () => {
+    const webcamRef = useRef<Webcam>(null);
+    const canvasRef = useRef(null);
 
-    return <div>this is webcam</div>;
+    // const detectWebcamFeed = async() = {
+    //     if(!webcamRef.current) {
+    //         const video = webcamRef.current.video;
+    //         const videoWidth = webcamRef.current.video.videoWidth;
+    //         const videoHeight = webcamRef.current.video.videoHeight;
+
+    //         webcamRef.current.video.width = videoWidth;
+    //         webcamRef.current.video.height = videoHeight;
+    //     },
+    // };
+
+    const detectWebcamFeed = async () => {
+        if (webcamRef.current && webcamRef.current.video) {
+            const video = webcamRef.current.video;
+            const videoWidth = webcamRef.current.video?.videoWidth;
+            const videoHeight = webcamRef.current.video?.videoHeight;
+
+            webcamRef.current.video.width = videoWidth;
+            webcamRef.current.video.height = videoHeight;
+        }
+    };
+
+    return (
+        <header className="App-header">
+            <Webcam
+                ref={webcamRef}
+                style={{
+                    position: "absolute",
+                    marginLeft: "auto",
+                    marginRight: "auto",
+                    left: 0,
+                    right: 0,
+                    textAlign: "center",
+                    width: 640,
+                    height: 480,
+                }}
+            />
+
+            <canvas
+                ref={canvasRef}
+                style={{
+                    position: "absolute",
+                    marginLeft: "auto",
+                    marginRight: "auto",
+                    left: 0,
+                    right: 0,
+                    textAlign: "center",
+                    width: 640,
+                    height: 480,
+                }}
+            />
+        </header>
+    );
 };
 
 export default WebcamWithPosenet;
