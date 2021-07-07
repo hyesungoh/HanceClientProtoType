afe32dba1302301ae712fd52e67a92fe0bb7fbf6
hyesung oh <haesungoh414@gmail.com>
2021-07-07 23:04:07 +0900

    feat: create WebcamWithPosenet component for using posenet with webcam

--- prototypefrontend/src/App.tsx
+++ prototypefrontend/src/App.tsx
@@ -1,16 +1,10 @@
 import React from "react";
+import WebcamWithPosenet from "Components/WebcamWithPosenet";
 
 function App() {
     return (
         <div>
             <h1>Prototype</h1>
-            <video
-                autoPlay
-                playsInline
-                muted={true}
-                height="100vh"
-                width="100vw"
-            ></video>
         </div>
     );
 }
---
+++ prototypefrontend/src/Components/WebcamWithPosenet.tsx
@@ -0,0 +1,21 @@
+// async function estimatePoseOnImage(imageElement) {
+//     // load the posenet model from a checkpoint
+//     const net = await posenet.load();
+
+//     const pose = await net.estimateSinglePose(imageElement, {
+//       flipHorizontal: false
+//     });
+//     return pose;
+//   }
+
+//   const imageElement = document.getElementById('cat');
+
+//   const pose = estimatePoseOnImage(imageElement);
+
+//   console.log(pose);
+
+const WebcamWithPosenet = () => {
+    return <div></div>;
+};
+
+export default WebcamWithPosenet;
