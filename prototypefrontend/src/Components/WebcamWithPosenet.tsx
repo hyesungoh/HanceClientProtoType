c67522a93823b82b3de2a7e92deef1ef4cbb2fd2
hyesung oh <haesungoh414@gmail.com>
2021-07-08 00:19:39 +0900

    feat: apply runPosenet function what load posenet and detect from webcam

--- prototypefrontend/src/Components/WebcamWithPosenet.tsx
+++ prototypefrontend/src/Components/WebcamWithPosenet.tsx
@@ -3,31 +3,40 @@ import { useRef } from "react";
 import * as tf from "@tensorflow/tfjs";
 import * as posenet from "@tensorflow-models/posenet";
 import Webcam from "react-webcam";
+import { useEffect } from "react";
 
 const WebcamWithPosenet = () => {
     const webcamRef = useRef<Webcam>(null);
     const canvasRef = useRef(null);
+    
+    useEffect(() => {
+        runPosenet();
+    }, []);
 
-    // const detectWebcamFeed = async() = {
-    //     if(!webcamRef.current) {
-    //         const video = webcamRef.current.video;
-    //         const videoWidth = webcamRef.current.video.videoWidth;
-    //         const videoHeight = webcamRef.current.video.videoHeight;
-
-    //         webcamRef.current.video.width = videoWidth;
-    //         webcamRef.current.video.height = videoHeight;
-    //     },
-    // };
-
-    const detectWebcamFeed = async () => {
-        if (webcamRef.current && webcamRef.current.video) {
+    const detectWebcamFeed = async (posenetModel: posenet.PoseNet) => {
+        if (webcamRef.current && webcamRef.current.video?.readyState === 4) {
             const video = webcamRef.current.video;
             const videoWidth = webcamRef.current.video?.videoWidth;
             const videoHeight = webcamRef.current.video?.videoHeight;
 
             webcamRef.current.video.width = videoWidth;
             webcamRef.current.video.height = videoHeight;
+
+            const pose = await posenetModel.estimateSinglePose(video);
+            console.log(pose.keypoints);
         }
+    };
+
+    const runPosenet = async () => {
+        const posenetModel = await posenet.load({
+            inputResolution: { width: 640, height: 480 },
+            architecture: "MobileNetV1",
+            outputStride: 16,
+        });
+
+        setInterval(() => {
+            detectWebcamFeed(posenetModel);
+        }, 5000);
     };
 
     return (
