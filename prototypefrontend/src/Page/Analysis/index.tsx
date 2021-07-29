15cc480f897628b492a5f732b7bdef094cfe5887
hyesung oh <haesungoh414@gmail.com>
2021-07-29 23:03:36 +0900

    feat: embed youtube video to analysis page

--- prototypefrontend/src/Page/Analysis/index.tsx
+++ prototypefrontend/src/Page/Analysis/index.tsx
@@ -5,7 +5,12 @@ import { video2Src } from "Static/Video/10.mp4";
 const Analysis = () => {
     return (
         <Wrapper>
-            <ReactPlayer url="https://www.youtube.com/watch?v=5HdXsaQf94k" />
+            <VideoWrapper>
+                <ReactPlayer
+                    url="https://www.youtube.com/watch?v=5HdXsaQf94k"
+                    controls
+                />
+            </VideoWrapper>
         </Wrapper>
     );
 };
@@ -15,4 +20,19 @@ export default Analysis;
 const Wrapper = styled.main`
     width: 100vw;
     height: 100vh;
+
+    display: flex;
+`;
+
+const VideoWrapper = styled.div`
+    position: relative;
+    width: 500px;
+    height: 100vh;
+    overflow: hidden;
+
+    & > * {
+        position: absolute;
+        width: 100% !important;
+        height: 100% !important;
+    }
 `;
