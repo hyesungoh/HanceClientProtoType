7565ea3f91a90c90fe9d185d8d7b5de11207f0c3
hyesung oh <haesungoh414@gmail.com>
2021-07-29 23:41:52 +0900

    feat: create Controllers component

---
+++ prototypefrontend/src/Components/Analysis/Controller.tsx
@@ -0,0 +1,7 @@
+import { Slider } from "@material-ui/core";
+
+const Controllers = () => {
+    return <Slider />;
+};
+
+export default Controllers;
--- prototypefrontend/src/Page/Analysis/index.tsx
+++ prototypefrontend/src/Page/Analysis/index.tsx
@@ -1,16 +1,44 @@
+import { useRef } from "react";
 import styled from "styled-components";
 import ReactPlayer from "react-player";
-import { video2Src } from "Static/Video/10.mp4";
+
+import weride2VideoSrc from "Static/Video/weride2.mp4";
+import Controllers from "Components/Analysis/Controller";
 
 const Analysis = () => {
+    const youtubeRef = useRef<ReactPlayer>(null);
     return (
         <Wrapper>
-            <VideoWrapper>
-                <ReactPlayer
-                    url="https://www.youtube.com/watch?v=5HdXsaQf94k"
-                    controls
-                />
-            </VideoWrapper>
+            <VideoSection>
+                <VideoWrapper>
+                    <ReactPlayer
+                        ref={youtubeRef}
+                        url="https://www.youtube.com/watch?v=SZ0UU3Ud3m0"
+                        playing
+                        progressInterval={200}
+                        volume={0}
+                        // onProgress={(e) => {
+                        //     console.log(e);
+                        // }}
+                    />
+                </VideoWrapper>
+                <VideoWrapper>
+                    <ReactPlayer
+                        ref={youtubeRef}
+                        url={weride2VideoSrc}
+                        playing
+                        progressInterval={200}
+                        volume={0}
+                        // onProgress={(e) => {
+                        //     console.log(e);
+                        // }}
+                    />
+                </VideoWrapper>
+            </VideoSection>
+
+            <ControllerWrapper>
+                <Controllers />
+            </ControllerWrapper>
         </Wrapper>
     );
 };
@@ -22,12 +50,20 @@ const Wrapper = styled.main`
     height: 100vh;
 
     display: flex;
+    align-items: center;
+    flex-direction: column;
+`;
+
+const VideoSection = styled.section`
+    width: 100%;
+    display: flex;
+    justify-content: center;
 `;
 
 const VideoWrapper = styled.div`
     position: relative;
     width: 500px;
-    height: 100vh;
+    height: 80vh;
     overflow: hidden;
 
     & > * {
@@ -35,4 +71,8 @@ const VideoWrapper = styled.div`
         width: 100% !important;
         height: 100% !important;
     }
+`;
+
+const ControllerWrapper = styled.div`
+    width: 80vw;
 `;
