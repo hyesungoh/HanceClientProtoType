828a02c4e1788052ff98976f48f285adecbc36c4
hyesung oh <haesungoh414@gmail.com>
2021-08-03 00:19:18 +0900

    feat: apply youtube video and webcam to MyOwn page

--- prototypefrontend/src/Page/Bring/index.tsx
+++ prototypefrontend/src/Page/Bring/index.tsx
@@ -1,15 +1,41 @@
 import styled from "styled-components";
-import { useState } from "react";
+import { useState, useRef, CSSProperties } from "react";
 import BeforeSet from "Components/MyOwn/BeforeSet";
+import Webcam from "react-webcam";
+import ReactPlayer from "react-player";
 
 const MyOwn = () => {
     const [isSet, setIsSet] = useState<boolean>(false);
     const [videoUrl, setVideoUrl] = useState<string>("");
 
+    const webcamRef = useRef<Webcam>(null);
+
+    const WebcamStyle: CSSProperties = {
+        position: "absolute",
+        top: "0",
+        left: "0",
+        width: "100%",
+        height: "100%",
+        objectFit: "cover",
+    };
+
+    // test video url
+    // https://www.youtube.com/watch?v=-cF7qRKjsXE
+
     if (!isSet)
         return <BeforeSet setIsSet={setIsSet} setVideoUrl={setVideoUrl} />;
 
-    return <Wrapper></Wrapper>;
+    return (
+        <Wrapper>
+            <YoutubeWrapper>
+                <ReactPlayer url={videoUrl} controls />
+            </YoutubeWrapper>
+
+            <WebcamWrapper>
+                <Webcam ref={webcamRef} style={WebcamStyle} />
+            </WebcamWrapper>
+        </Wrapper>
+    );
 };
 
 export default MyOwn;
@@ -19,4 +45,27 @@ const Wrapper = styled.div`
     width: 100vw;
     height: 100vh;
     display: flex;
+`;
+
+const YoutubeWrapper = styled.div`
+    position: relative;
+    width: 50%;
+    height: 100vh;
+    overflow: hidden;
+
+    & > * {
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100% !important;
+        height: 100% !important;
+    }
+`;
+
+const WebcamWrapper = styled.div`
+    position: relative;
+    width: 50%;
+    height: 100vh;
+
+    overflow: hidden;
 `;
