e61f9a587855185e367bdf7b63ccb5f4de309877
hyesung oh <haesungoh414@gmail.com>
2021-08-05 01:31:37 +0900

    feat: apply gauge bar effect

--- prototypefrontend/src/Components/Learning/Gauge.tsx
+++ prototypefrontend/src/Components/Learning/Gauge.tsx
@@ -1,18 +1,20 @@
+import { useState } from "react";
 import { useEffect } from "react";
 import { useRecoilValue } from "recoil";
 import { compareCurrentMeanScoreState } from "Store";
 import styled from "styled-components";
 
 const Gauge = () => {
+    const [gaugeHeight, setGaugeHeight] = useState<number>(0);
     const currentMeanScore = useRecoilValue(compareCurrentMeanScoreState);
 
     useEffect(() => {
-        console.log(currentMeanScore);
+        setGaugeHeight((currentMeanScore - 60) * 2.5);
     }, [currentMeanScore]);
 
     return (
         <Wrapper>
-            <GaugeBar />
+            <GaugeBar gaugeHeight={gaugeHeight} />
         </Wrapper>
     );
 };
@@ -28,8 +30,17 @@ const Wrapper = styled.div`
     align-items: flex-end;
 `;
 
-const GaugeBar = styled.div`
+const GaugeBar = styled.div<{ gaugeHeight: number }>`
     width: 100%;
     height: 100%;
-    background-color: red;
+    background-color: ${({ gaugeHeight }) => {
+        if (gaugeHeight > 80) return "blue";
+        else if (gaugeHeight > 50) return "green";
+        else if (gaugeHeight > 30) return "orange";
+        else return "red";
+    }};
+
+    transition: transform 0.9s, background-color 0.9s;
+    transform-origin: bottom;
+    transform: ${({ gaugeHeight }) => `scaleY(${gaugeHeight}%)`};
 `;
